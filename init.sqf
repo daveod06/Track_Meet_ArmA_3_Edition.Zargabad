9f7a69750afb9135987dd53c8baf75e093ef7d41
daveod06 <davidodonnell88@gmail.com>
2017-11-27 00:25:45 -0700

    removed some music; changed waves, modified defend are

--- init.sqf
+++ init.sqf
@@ -10,3 +10,12 @@ detonateBombs = false;
 //iedMkr=["iedMkr0","iedMkr1","iedMkr2"];
 //[iedMkr,detonateBombs] execVM "scripts\ied.sqf";
 //[] execVM "scripts\ied.sqf";
+
+0 = [] spawn
+{
+while{true}do
+{
+playMusic "01";
+sleep 140.0; //where xyz the duration of the music file is
+};
+};
--- music/Music.hpp
+++ music/Music.hpp
@@ -1,40 +1,10 @@
 class CfgMusic
 {
-	sounds[] = {01,02,03,04,05,06,07};
+	sounds[] = {01};
 	class 01
-	{
-		name = "007_Dam_Music";
-		sound[] = {"music\007_Dam.ogg", db+10, 1.0};
-		titles[] = {0,""};
-	};
-	class 02
 	{
 		name = "007_Facility_Music";
 		sound[] = {"music\007_Facility.ogg", db+10, 1.0};
-		titles[] = {0,""};
-	};
-	class 03
-	{
-		name = "007_Egyptian_Music";
-		sound[] = {"music\007_Egyptian.ogg", db+10, 1.0};
-		titles[] = {0,""};
-	};
-	class 04
-	{
-		name = "007_Cradle_Music";
-		sound[] = {"music\007_Cradle.ogg", db+10, 1.0};
-		titles[] = {0,""};
-	};
-	class 06
-	{
-		name = "007_Bunker_2_Music";
-		sound[] = {"music\007_Bunker_2.ogg", db+10, 1.0};
-		titles[] = {0,""};
-	};
-	class 07
-	{
-		name = "007_Surface_2_Music";
-		sound[] = {"music\007_Surface_2.ogg", db+10, 1.0};
 		titles[] = {0,""};
 	};
 };
